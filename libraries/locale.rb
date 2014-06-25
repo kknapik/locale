0bcefd96a9d63c420de2791a838e8f9073e34ec9
Krzysztof Knapik <knapo@knapo.net>
2014-06-25 11:22:10 +0200

    Fix `Locale.up_to_date?` to handle case when locale file not exists.

--- libraries/locale.rb
+++ libraries/locale.rb
@@ -1,6 +1,7 @@
 module Locale
   class << self
     def up_to_date?(file_path, lang, lc_all)
+      return false unless File.exists?(file_path)
       locale = IO.read(file_path)
       locale.include?("LANG=#{lang}") && locale.include?("LC_ALL=#{lc_all}")
     end
